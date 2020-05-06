f15f1c62ea5ce698e53b4f9acec6ac6e2dfa0173
MarioDuarthe <duarthe0@hotmail.com>
2020-05-05 23:34:32 -0300

    Layout do sistema e primeira ação da tela

---
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            string nome = HttpContext.Request.Form["nome"];
+            string email= HttpContext.Request.Form["email"];
+            string texto = HttpContext.Request.Form["texto"];
+
+
+            return new ContentResult() { Content = String.Format("Contatos recebido ! <br/> Nome:{0} <br/> Email:{1} <br/> Texto:{2}",nome, email,texto), ContentType="text/html" };
+        }
+    }
+}
